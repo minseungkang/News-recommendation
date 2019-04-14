6d88bb2851109150d51ed168b9eac9562aa491bb
minseungkang <alstmd3@naver.com>
2019-04-14 20:30:06 +0900

    Add InitUser test

--- src/test/java/test/UserDAOTest.java
+++ src/test/java/test/UserDAOTest.java
@@ -7,10 +7,15 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import weonforall.capstone.news_recommendation.controller.User;
 import weonforall.capstone.news_recommendation.domain.UserVO;
+import weonforall.capstone.news_recommendation.enums.Status;
+import weonforall.capstone.news_recommendation.model.Result;
 import weonforall.capstone.news_recommendation.persistence.IUserDAO;
 
 import java.sql.Time;
+import java.util.HashMap;
+import java.util.UUID;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,19 +23,38 @@ import java.sql.Time;
 public class UserDAOTest {
 
     @Autowired
-    private IUserDAO IUserDAO;
+    private IUserDAO iUserDAO;
 
     @Test
     public void Order1_InsertUser() throws Exception {
-        UserVO userVO = new UserVO();
-        userVO.setUid("usertest00");
-        userVO.setPushTime(new Time(12));
+        String uid = UUID.randomUUID().toString();
 
-        IUserDAO.insertUser(userVO);
+        Result result;
+        int maximumLoopCnt = 100;
+        int loopCnt = 0;
+        while (loopCnt < maximumLoopCnt) {
+            uid = UUID.randomUUID().toString();
+
+            // check uid exists in db
+            if (iUserDAO.checkUUid(uid) == 0) break;
+            loopCnt++;
+        }
+
+        if (loopCnt >= maximumLoopCnt) {
+            result = new Result(Status.Key.INVALID, Status.Obj.USER, "uid", uid);
+        }
+
+        UserVO userVO = new UserVO();
+        userVO.setUid(uid);
+        userVO.setPushTime(new Time(0));
+        iUserDAO.insertUser(userVO);
+
+        result = new Result(Status.Key.SUCCEED, Status.Obj.REQUEST, "uservo", userVO);
+        System.out.println(result);
     }
 
     @Test
     public void Order2_GetUser() throws Exception {
-        System.out.println(IUserDAO.getUser("usertest00"));
+        System.out.println(iUserDAO.getUser("usertest00"));
     }
 }
