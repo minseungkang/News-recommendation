815c6d4c9a5521ae92147d60b9a6b2a20202548b
minseungkang <alstmd3@naver.com>
2019-04-14 15:07:11 +0900

    Article separate read API
    to read / feedback API

--- src/main/java/weonforall/capstone/news_recommendation/controller/Article.java
+++ src/main/java/weonforall/capstone/news_recommendation/controller/Article.java
@@ -32,7 +32,6 @@ public class Article {
     public @ResponseBody Result read(@RequestBody Map<String, Object> param) {
         String uid = (String)param.getOrDefault("uid", null);
         Long aid = Integer.toUnsignedLong((int)param.getOrDefault("aid", -1));
-        Boolean feedback = (Boolean)param.getOrDefault("feedback", null);
 
         Result result = null;
 
@@ -50,7 +49,6 @@ public class Article {
             }
 
             ArticleHistoryVO articleHistoryVO = iArticleHistoryDAO.getArticleHistory(aid, uid);
-            articleHistoryVO.setFeedback(feedback);
             articleHistoryVO.setRead(true);
             iArticleHistoryDAO.updateArticleHistory(articleHistoryVO);
 
@@ -93,6 +91,39 @@ public class Article {
             result = new Result(Status.Key.UNKNOWN, Status.Obj.ERROR);
         }
 
+        return result;
+    }
+
+
+    @RequestMapping(value = "/article/feedback", method = RequestMethod.POST)
+    public @ResponseBody Result feedback(@RequestBody Map<String, Object> param) {
+        String uid = (String) param.getOrDefault("uid", null);
+        Long aid = Integer.toUnsignedLong((int)param.getOrDefault("aid", -1));
+        Boolean feedback = (Boolean)param.getOrDefault(("feedback"), null);
+
+        Result result = null;
+
+        try {
+            ArticleVO articleVO = iArticleDAO.getArticle(aid);
+            if (articleVO == null) {
+                result = new Result(Status.Key.NOT_EXIST, Status.Obj.ARTICLE);
+                return result;
+            }
+
+            UserVO userVO = iUserDAO.getUser(uid);
+            if(userVO == null) {
+                result = new Result(Status.Key.NOT_EXIST, Status.Obj.USER);
+                return result;
+            }
+
+            ArticleHistoryVO articleHistoryVO = iArticleHistoryDAO.getArticleHistory(aid, uid);
+            articleHistoryVO.setFeedback(feedback);
+            iArticleHistoryDAO.updateArticleHistory(articleHistoryVO);
+
+            result = new Result(Status.Key.SUCCEED, Status.Obj.REQUEST, "article", articleHistoryVO);
+        } catch (Exception e) {
+            result = new Result(Status.Key.UNKNOWN, Status.Obj.ERROR);
+        }
         return result;
     }
 
