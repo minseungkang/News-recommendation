9a6db98d8c9dd7a58aa5aa4aca2e67d4f6070977
minseungkang <alstmd3@naver.com>
2019-04-29 21:44:44 +0900

    Add push by cron

--- src/main/java/weonforall/capstone/news_recommendation/Service/PushService.java
+++ src/main/java/weonforall/capstone/news_recommendation/Service/PushService.java
@@ -1,4 +1,14 @@
 package weonforall.capstone.news_recommendation.Service;
 
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PushService {
+
+    @Scheduled(cron = "0 0/5 * * * *")
+    public void TestScheduler() {
+        System.out.println("scheduling test");
+    }
 }
